56a35bd9ab5a805ea59337f2f7d0ce37c5e6f4c7
abrahamgobimbel <abraham9haan.go@gmail.com>
2023-11-20 16:50:13 +0700

    .

--- function_activator.py
+++ function_activator.py
@@ -15,23 +15,13 @@ def old_database_name() :
     old_database_name = function_choice.old_database_name(old_database_name)
     return old_database_name
 
-def database_name(old_database_name) :
-    database_name = function_table.database_name(old_database_name)
-    database_name = function_choice.database_name(database_name)
-    return database_name
-
 def old_table_name(old_database_name) :
     old_table_name = function_table.old_table_name(old_database_name)
     old_table_name = function_choice.old_table_name(old_table_name)
     return old_table_name
 
-def table_name(database_name) :
-    table_name = function_table.table_name(database_name)
-    table_name = function_choice.table_name(table_name)
-    return table_name
-
-def to_excel(old_database_name, table_name, database_name, sql_query):
-    function_to_excel.to_excel(old_database_name, table_name, database_name, sql_query)
+def to_excel(old_database_name, old_table_name, sql_query):
+    function_to_excel.to_excel(old_database_name, old_table_name, sql_query)
     
 
 def sql_query(old_table_name) :
--- read_sql_function.py
+++ read_sql_function.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 import os
 
-
-
 def clear_terminal():
     os.system("cls")
 all_data = []
@@ -14,19 +12,13 @@ old_database_name = function_activator.old_database_name()
 all_data.append(f"old_database_name : {old_database_name}")
 clear_terminal()
 old_table_name = function_activator.old_table_name(old_database_name)  
-all_data.append(f"old_table_name : {old_table_name}")  
-clear_terminal()
-database_name = function_activator.database_name(old_database_name)
-all_data.append(f"database_name : {database_name}")
-clear_terminal()
-table_name = function_activator.table_name(database_name)  
-all_data.append(f"table_name : {table_name}")  
+all_data.append(f"old_table_name : {old_table_name}")   
 clear_terminal()
 for i in range(len(all_data)) : print(all_data[i])
 sql_query = function_activator.sql_query(old_table_name)
-function_activator.to_excel(old_database_name, table_name, database_name, sql_query)
+function_activator.to_excel(old_database_name, old_table_name, sql_query)
 clear_terminal()
-print (f"Excel {database_name}_{table_name} sudah selesai")
+print (f"Excel {old_database_name}_{old_table_name} sudah selesai")
 
 from datetime import date
 def date_today() :
@@ -35,8 +27,8 @@ def date_today() :
     return date_today
 
 date_today_value = date_today()   
-nama_file_excel = f'{table_name}_{date_today_value}.xlsx'
-directory_path = Path(f"{database_name}/excel")
+nama_file_excel = f'{old_database_name}_{old_table_name}_{date_today_value}.xlsx'
+directory_path = Path(f"{old_database_name}/excel")
 file_path = Path(os.path.join(directory_path, nama_file_excel))
 data_excel = pd.read_excel(file_path)
 
